2c31f26cedba06211afcf3a8a9832cdb81d9854e
johnobodai <j.obodai@alustudent.com>
2022-10-11 09:33:04 +0000

    Updated

---
+++ meow.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int counter = 3;
+    int i = 3;
+    while (counter > 0)
+    {
+        printf("meow\n");
+        counter--;
+    }
+
+    printf("\n");
+
+    while (i > 0)
+    {
+        printf("meow\n");
+            i--;
+    }
+    
+    printf("\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
+
+
+}
