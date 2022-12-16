faf3053825665ec8575f81b71db5d4d8b7c128b2
johnobodai <j.obodai@alustudent.com>
2022-12-16 07:07:15 +0000

    sdf

--- Python/automateTheBoringStuffWithPython/Part2/phoneAndEmail.py
+++ Python/automateTheBoringStuffWithPython/Part2/phoneAndEmail.py
@@ -13,6 +13,12 @@ phoneRegex = re.compile(r'''(
     )''',re.VERBOSE)
 
 # Create email regex.
+emailRegec = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+   # username
+    @                   # @ symbol
+    [a-zA-Z0-9]+        # domain name
+    (\.[a-zA-Z]{2,4})   # dot-something
+    )''', re.VERBOSE)
 
 # TODO: Find matchess in clipboard text.
 # TODO: Copy results to the clipboard
