17fdeacbecf84df12efbf72fd80c1c3d990d8261
johnobodai <j.obodai@alustudent.com>
2022-12-20 09:10:21 +0000

    validation

---
+++ Python/validate_mary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+name = 'Mary'
+password = 'swordfish'
+if name == 'Mary':
+    print('Hello, Mary')
+    if password == 'swordfish':
+        print('Access granted.')
+    else:
+        print('Wrong password.')
+
