700b836180098029c3af2d587ace62e15164d5da
johnobodai <j.obodai@alustudent.com>
2022-12-20 10:03:20 +0000

    game

---
+++ Python/guessTheNumber.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+ 
+# This is guess the number game.
+import random
+secretNumber = random.randit(1, 20)
+print('I am thinking of a number between 1 and 20.')
+
+# Ast the player to guess 6 times.
+for guessTaken in range(1, 7):
+    print('Take a guess.')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break   # This condition is the correct guess
