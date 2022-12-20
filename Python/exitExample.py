2f275d9eb30262253f69dce5afa8accc1da5a1fb
johnobodai <j.obodai@alustudent.com>
2022-12-20 09:56:17 +0000

    end now

--- Python/exitExample.py
+++ Python/exitExample.py
@@ -6,6 +6,6 @@ import sys
 while True:
     print('Type exit to exit.')
     response = input()
-    if response = 'exit':
+    if response == 'exit':
         sys.exit()
     print('You typed' + response + '.')
