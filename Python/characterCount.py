fd02fc3fd3929e960487b2df6b55589a3deb5c99
johnobodai <j.obodai@alustudent.com>
2022-12-21 07:47:16 +0000

    count us

--- Python/characterCount.py
+++ Python/characterCount.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Counts the number of individual characters in a string inputed my a user:
-
+import pprint
 
 message = input('Input your string to be counted here: ')
 count = {}
@@ -10,3 +10,4 @@ for character in message:
     count[character] = count[character] + 1
 
 print(count)
+pprint.pprint(count)
