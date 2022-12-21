07df85f4bfe7061cdee143665709029cff6d0be8
johnobodai <j.obodai@alustudent.com>
2022-12-21 06:49:47 +0000

    sdf

--- Python/conway.py
+++ Python/conway.py
@@ -10,8 +10,8 @@ nextCells = []
 for x in range(WIDTH):
     column = [] # Create a column
     for y in range(HEIGHT):
-        if random.randint(0, 1) == 0;
-        column.append('#') # Add a living cell.
+        if random.randint(0, 1) == 0:
+            column.append('#') # Add a living cell.
     else:
         column.append(' ') # Add a dead cell.
     nextCells.append(column) # nextCells is a list of column lists.
