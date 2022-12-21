8adc911be0eb121b9f64fd6bb8d4b1ee22ac71ce
johnobodai <j.obodai@alustudent.com>
2022-12-21 06:58:22 +0000

    sdf

--- Python/conway.py
+++ Python/conway.py
@@ -25,3 +25,20 @@ while True: # Main program loop.
         for x in range(WIDTH):
             print(currentCells[x][y], end='') # Print the # or space.
         print() # Print a newline at the end of the row.
+
+    # Calculate the next step's cells based on current step's cells:
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates:
+            # '% WIDTH' ensures leftCoord is always between 0 and WIDTH - 1
+            leftCoord = (x - 1) % WIDTH
+            rightCoord = (x + 1) % WIDTH
+            aboveCoord = (y - 1) % HEIGHT
+            belowCoord = (y + 1) % HEIGHT
+
+            # Count numbers of living neightbors:
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-left neighbor is alive
+            if currentCells[x][aboveCoord] == '#'
+            numNeighbors += 1 # Top neighbor is alive
