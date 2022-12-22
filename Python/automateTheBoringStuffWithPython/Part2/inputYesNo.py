fca16406e8c9a62dfe29f2750d19eafb8e8ede41
johnobodai <j.obodai@alustudent.com>
2022-12-22 14:59:45 +0000

    keep busy

--- Python/automateTheBoringStuffWithPython/Part2/inputYesNo.py
+++ Python/automateTheBoringStuffWithPython/Part2/inputYesNo.py
@@ -4,3 +4,6 @@ import pyinputplus as pyin
 while True:
     prompt = 'What to know how to keep an idiot busy for hours?\n'
     response = pyin.inputYesNo(prompt)
+    if response == 'no':
+        break
+print('Thank you, Have a nice day.')
