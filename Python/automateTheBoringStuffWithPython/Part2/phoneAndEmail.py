c0071fe845f76b492d4f6a821fc013fda1430a3b
johnobodai <j.obodai@alustudent.com>
2022-12-22 14:25:14 +0000

    df

--- Python/automateTheBoringStuffWithPython/Part2/phoneAndEmail.py
+++ Python/automateTheBoringStuffWithPython/Part2/phoneAndEmail.py
@@ -32,4 +32,10 @@ for groups om phoneRegex.findall(text):
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
 
-# TODO: Copy results to the clipboard
+# Copy results to the clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
