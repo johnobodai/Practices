d5fad733eba9f82fb76885211d2c5f5da9cd1ccc
johnobodai <j.obodai@alustudent.com>
2022-12-22 15:16:22 +0000

    mulQuiz

--- Python/automateTheBoringStuffWithPython/Part2/multiplicationQuiz.py
+++ Python/automateTheBoringStuffWithPython/Part2/multiplicationQuiz.py
@@ -17,4 +17,14 @@ for questionNumber in range(numberOfQuestions):
         pyip.inputStr(prompt, allowRegexes=['^%s$' % (num1 * num2)],
                               blockRegexes=[('.*', 'Incorrect!')],
                               timeout=8, limit=3)
+    except pyip.TimeoutException:
+        print('Out of time')
+    except pyip.RetryLimitException:
+        print('Out of tries!')
+    else:
+        #This block runs in no exception were raised in the try block.
+        print('Correct!')
+        correctAnswers += 1
+    time.sleep(1)  # Brief pause to let user see the result.
+    print('Score: {} / {}'.format(correctAnswers, numberOfQuestions))
 
