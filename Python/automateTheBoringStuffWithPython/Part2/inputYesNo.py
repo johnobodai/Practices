2fd5cd7b7be5c39c655ab111e84bf7ad30331d2e
johnobodai <j.obodai@alustudent.com>
2022-12-22 14:54:00 +0000

    dsf

--- Python/automateTheBoringStuffWithPython/Part2/inputYesNo.py
+++ Python/automateTheBoringStuffWithPython/Part2/inputYesNo.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+# Program to keep an idiot busy for hours
+import pyinputplus as pyin
+while True:
+    prompt = 'What to know how to keep an idiot busy for hours?\n'
+    response = pyin.inputYesNo(prompt)
