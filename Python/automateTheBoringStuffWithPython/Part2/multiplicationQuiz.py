9a4567f79fe485ac194977cb6a5aa6d66aee0243
johnobodai <j.obodai@alustudent.com>
2022-12-26 08:59:22 +0000

    spam file

--- Python/automateTheBoringStuffWithPython/Part2/multiplicationQuiz.py
+++ Python/automateTheBoringStuffWithPython/Part2/multiplicationQuiz.py
@@ -9,7 +9,7 @@ for questionNumber in range(numberOfQuestions):
     num1 = random.randint(0, 9)
     num2 = random.randint(0, 9)
 
-    prompt = '#{}: {} x {}.'.format(questionNumber, num1, num2)
+    prompt = '#{}: {} x {} = '.format(questionNumber, num1, num2)
 
     try:
         # Right answers are handled by allowRegexes.
