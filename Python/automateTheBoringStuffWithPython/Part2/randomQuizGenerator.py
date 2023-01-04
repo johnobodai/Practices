742d7e17de4f4315e463cf2726d3755f5d8e6fbc
johnobodai <j.obodai@alustudent.com>
2023-01-04 06:37:44 +0000

    store the quiz data in a dictionary

---
+++ Python/automateTheBoringStuffWithPython/Part2/randomQuizGenerator.py
@@ -0,0 +1,6 @@
+#! usr/bin/python3
+# ramdomQuizGenerator.py - Creates quizzes with questions and answers in 
+# random order, along with the answer key.
+
+import random
+# The quiz data. Keys are states and values are their capitals.
