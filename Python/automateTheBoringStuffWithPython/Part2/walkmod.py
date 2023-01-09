b791c10c02f637f8b74d3cb5c1d0f4d8343b6b24
johnobodai <j.obodai@alustudent.com>
2023-01-09 07:54:58 +0000

    lists all fol and sub fols

--- Python/automateTheBoringStuffWithPython/Part2/walkmod.py
+++ Python/automateTheBoringStuffWithPython/Part2/walkmod.py
@@ -1,7 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+# this script lists all the folds, subfolders and filename in the home dir
 
-import os, pathlib
-for folderName, subfolders, filenames in os.walk(path.home()):
+import os
+from pathlib import Path
+for folderName, subfolders, filenames in os.walk(Path.home()):
     print('The current folder is ' + folderName)
 
     for subfolder in subfolders:
